03f49b2fc7924d022ffd276c94f6417001bd21be
Trungnef <cauchubebong184@gmail.com>
2025-07-02 02:10:13 +0700

    Fix err Docker server

--- src/server.ts
+++ src/server.ts
@@ -139,7 +139,7 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
       secure: process.env.NODE_ENV === "production", // Only use HTTPS in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-      domain: process.env.COOKIE_DOMAIN || undefined // allow cross-sub-domain cookies if needed
+      domain: process.env.COOKIE_DOMAIN || (process.env.NODE_ENV === 'production' ? '.datacorpsolutions.com' : undefined)
     },
     name: "sessionId", // Custom session name for security
   })
